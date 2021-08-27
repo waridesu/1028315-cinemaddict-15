d0ea854b58c391ddd96d72a464ed30c67ebe3c38
Denis <waridesu@gmail.com>
2021-08-27 14:44:16 +0300

    module 3.2:
    - code review

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ let popUpComponent;
 const closePopUpComponent = () => {
   popUpComponent.getElement().remove();
   document.body.classList.remove('hide-overflow');
+  popUpComponent.getElement().querySelector('.film-details__close-btn').removeEventListener('click', closePopUpComponent);
   popUpComponent = null;
 
 };
