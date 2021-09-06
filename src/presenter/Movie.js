0f0682d87fa1f44abb7a9612d16ff6a2ff805b66
Denis <waridesu@gmail.com>
2021-09-07 00:23:12 +0300

    module 5.1:
    - one working but not refresh popup

--- src/presenter/Movie.js
+++ src/presenter/Movie.js
@@ -1,21 +1,18 @@
 import SiteFilmCardView from '../view/site-film-container/site-film-card/site-film-card';
 import {remove, render, RenderPosition, replace} from '../view/utils/render';
-import SiteSitePopUpView from '../view/site-popout/site-popup';
-
 
 export default class Movie {
-  constructor(MovieListContainer, changeDate) {
+  constructor(MovieListContainer, rerenderData, renderPopup) {
     this._movieListContainer = MovieListContainer;
-    this._changeData = changeDate;
+    this._rerenderData = rerenderData;
+    this._renderPopup = renderPopup;
 
     this._movieComponent = null;
     this._prevMovieComponent = null;
-    this._sitePopUp = null;
     this._addToWatchList = this._addToWatchList.bind(this);
     this._alreadyWatched = this._alreadyWatched.bind(this);
     this._addToFavorite = this._addToFavorite.bind(this);
-    this._onEscKeyUp = this._onEscKeyUp.bind(this);
-    this._renderPopUp = this._renderPopUp.bind(this);
+    this._openPopup = this._openPopup.bind(this);
   }
 
   init(movie) {
@@ -25,7 +22,7 @@ export default class Movie {
 
     this._movieComponent = new SiteFilmCardView(movie);
 
-    this._movieComponent.setClickHandler(this._renderPopUp);
+    this._movieComponent.setClickHandler(this._openPopup);
     this._movieComponent.setAddToWatchListHandler(this._addToWatchList);
     this._movieComponent.setAlreadyWatchedHandler(this._alreadyWatched);
     this._movieComponent.setAddToFavoritesHandler(this._addToFavorite);
@@ -41,67 +38,52 @@ export default class Movie {
     remove(this._movieComponent);
   }
 
-  _renderPopUp() {
-    if (document.querySelector('.film-details')) {
-      this._closePopUp();
-    }
-    document.addEventListener('keyup', this._onEscKeyUp);
-    document.body.classList.add('hide-overflow');
-    this._sitePopUp = new SiteSitePopUpView(this._movie);
-    render(this._movieListContainer, this._sitePopUp, RenderPosition.BEFOREEND);
-
-    this._sitePopUp.setAddToWatchListHandler(()=> this._addToWatchList);
-    this._sitePopUp.setAlreadyWatchedHandler(()=> this._alreadyWatched);
-    this._sitePopUp.setAddToFavoritesHandler(()=> this._addToFavorite);
-
-    this._sitePopUp.setCloseButtonHandler(() => {
-      document.removeEventListener('keyup', this._onEscKeyUp);
-      this._closePopUp();
-    });
-  }
-
   _replaceMovie() {
     replace(this._movieComponent, this._prevMovieComponent);
-    document.removeEventListener('keydown', this._onEscKeyUp);
   }
 
   resetView() {
     this._replaceMovie();
   }
 
-  _closePopUp() {
-    remove(this._sitePopUp);
-    document.body.classList.remove('hide-overflow');
-    this._sitePopUp = null;
-  }
-
-  _onEscKeyUp(evt) {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      this._closePopUp();
-    }
+  _openPopup () {
+    this._renderPopup(this._movie, this._addToWatchList, this._alreadyWatched, this._addToFavorite);
   }
 
   _addToWatchList() {
-    this._changeData(
-      Object.assign({}, this._movie.user_details.watchlist =!this._movie.user_details.watchlist,
+    this._rerenderData(
+      Object.assign({}, this._movie, {
+        'user_details': {
+          watchlist: !this._movie.user_details.watchlist,
+          alreadyWatched: this._movie.user_details.alreadyWatched,
+          favorite: this._movie.user_details.favorite,
+        },
+      },
       ),
     );
   }
 
   _alreadyWatched() {
-    this._changeData(
-      Object.assign({}, this._movie.user_details, {
-        alreadyWatched: !this._movie.user_details.alreadyWatched,
+    this._rerenderData(
+      Object.assign({}, this._movie, {
+        'user_details': {
+          watchlist: this._movie.user_details.watchlist,
+          alreadyWatched: !this._movie.user_details.alreadyWatched,
+          favorite: this._movie.user_details.favorite,
+        },
       },
       ),
     );
   }
 
   _addToFavorite() {
-    this._changeData(
-      Object.assign({}, this._movie.user_details, {
-        alreadyWatched: !this._movie.user_details.alreadyWatched,
+    this._rerenderData(
+      Object.assign({}, this._movie, {
+        'user_details': {
+          watchlist: this._movie.user_details.watchlist,
+          alreadyWatched: this._movie.user_details.alreadyWatched,
+          favorite: !this._movie.user_details.favorite,
+        },
       },
       ),
     );
--- src/presenter/MovieList.js
+++ src/presenter/MovieList.js
@@ -1,13 +1,14 @@
 import SiteFilmContainerView from '../view/site-film-container/site-film-container.js';
 import SiteMoreButtonView from '../view/site-more-button.js';
 import SiteFilmListView from '../view/site-film-container/film-list/film-list.js';
-import {remove, render, RenderPosition} from '../view/utils/render';
+import {remove, render, RenderPosition, replace} from '../view/utils/render';
 import SiteSortView from '../view/site-sort';
 import FilmListTop from '../view/site-film-container/film-list-containers/film-section-top';
 import FilmListMost from '../view/site-film-container/film-list-containers/film-section-most';
 import FilmListSection from '../view/site-film-container/film-list-containers/film-section';
 import {updateItem} from '../view/utils/common';
 import MoviePresenter from './Movie';
+import SiteSitePopUpView from '../view/site-popout/site-popup';
 
 const FILM_COUNT_PER_STEP = 5;
 
@@ -18,6 +19,8 @@ export default class MovieList {
     this._moviePresenter = new Map();
     this._siteSortComponent = new SiteSortView();
     this._movieList = [];
+    this._sitePopUp = null;
+    this._prevSitePopUp = null;
     this._siteFilmContainerComponent = new SiteFilmContainerView();
     this._filmListSection = new FilmListSection();
     this._filmListMost = new FilmListMost();
@@ -26,7 +29,9 @@ export default class MovieList {
     this._filmListMostContainer = new SiteFilmListView();
     this._filmListTopContainer = new SiteFilmListView();
     this._moreButton = new SiteMoreButtonView();
-    this._handleTaskChange = this._handleTaskChange.bind(this);
+    this._handleMovieChange = this._handleMovieChange.bind(this);
+    this._renderPopUp = this._renderPopUp.bind(this);
+    this._onEscKeyUp = this._onEscKeyUp.bind(this);
   }
 
   init(listMovies) {
@@ -49,18 +54,50 @@ export default class MovieList {
     this._renderList();
   }
 
-  _handleTaskChange(updatedFilm) {
+  _handleMovieChange(updatedFilm) {
     this._movieList = updateItem(this._movieList, updatedFilm);
     this._sourcedMovieList = updateItem(this._sourcedMovieList, updatedFilm);
     this._moviePresenter.get(updatedFilm.id).init(updatedFilm);
   }
 
+  _renderPopUp(movie, addToWatchList, alreadyWatched, addToFavorite) {
+    if (this._sitePopUp) {
+      this._closePopUp();
+    }
+    document.addEventListener('keyup', this._onEscKeyUp);
+    document.body.classList.add('hide-overflow');
+    this._prevSitePopUp = this._sitePopUp;
+
+    this._sitePopUp = new SiteSitePopUpView(movie);
+
+    render(this._movieListContainer, this._sitePopUp, RenderPosition.BEFOREEND);
+
+    this._sitePopUp.setAddToWatchListHandler(addToWatchList);
+    this._sitePopUp.setAlreadyWatchedHandler(alreadyWatched);
+    this._sitePopUp.setAddToFavoritesHandler(addToFavorite);
+
+    this._sitePopUp.setCloseButtonHandler(() => {
+      this._closePopUp();
+    });
+
+    if(this._prevSitePopUp === null) {
+      return render(this._movieListContainer, this._sitePopUp, RenderPosition.BEFOREEND);
+    }
+    this._replacePopup();
+    remove(this._prevSitePopUp);
+  }
+
+  // как смог нужно перерисовка попапа
+  _replacePopup() {
+    replace(this._sitePopUp, this._prevSitePopUp);
+  }
+
   _renderSort() {}
 
   _renderMovie(movie) {
-    const taskPresenter = new MoviePresenter(this._filmListSectionContainer, this._handleTaskChange);
-    taskPresenter.init(movie);
-    this._moviePresenter.set(movie.id, taskPresenter);
+    const moviePresenter = new MoviePresenter(this._filmListSectionContainer, this._handleMovieChange, this._renderPopUp);
+    moviePresenter.init(movie);
+    this._moviePresenter.set(movie.id, moviePresenter);
   }
 
   _renderMovies(from, to) {
@@ -71,6 +108,20 @@ export default class MovieList {
 
   _renderLoadMoreButton() {
     render(this._filmListSection, this._moreButton, RenderPosition.BEFOREEND);
+  }
+
+  _closePopUp() {
+    remove(this._sitePopUp);
+    document.body.classList.remove('hide-overflow');
+    this._sitePopUp = null;
+    document.removeEventListener('keydown', this._onEscKeyUp);
+  }
+
+  _onEscKeyUp(evt) {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this._closePopUp();
+    }
   }
 
   _renderList() {
