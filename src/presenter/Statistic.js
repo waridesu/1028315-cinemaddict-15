8768481803c1b561d6394b8fe4d36e0e0d46240e
Denis <waridesu@gmail.com>
2021-09-22 21:25:19 +0300

    module 7.1:
    - code review

--- src/presenter/Statistic.js
+++ src/presenter/Statistic.js
@@ -17,6 +17,8 @@ export default class Statistic {
     if (prevSiteStatisticComponent === null) {
       return render(this._statisticContainer, this._siteStatisticComponent, RenderPosition.BEFOREEND);
     }
+    this._moviesModel.addObserver(this._handleModelEvent);
+    this._filterModel.addObserver(this._handleModelEvent);
     replace(this._siteStatisticComponent, prevSiteStatisticComponent);
     remove(prevSiteStatisticComponent);
   }
